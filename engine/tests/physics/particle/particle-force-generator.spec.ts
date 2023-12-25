9f15dc5974e11d096ac9bdf3c3079c875a053e7c
agulakhszyan <agulakhszyan@tradingview.com>
2023-12-25 17:27:55 +0300

    add spring tests

--- engine/tests/physics/particle/particle-force-generator.spec.ts
+++ engine/tests/physics/particle/particle-force-generator.spec.ts
@@ -2,14 +2,14 @@ import { beforeEach, describe, expect, it } from "@jest/globals";
 import { type IParticle, Particle, ParticleForceGenerator } from "../../../src/physics/particle";
 import { Vector2 } from "../../../src/math/vector2";
 
-describe("ParticleForceGenerator", () => {
-    const zeroVector = new Vector2();
+const zeroVector = new Vector2();
 
+describe("ParticleForceGenerator: Gravity", () => {
     beforeEach(() => {
         expect(zeroVector).toEqual(new Vector2(0, 0));
     });
 
-    it("should apply gravity to particles with finite mass", () => {
+    it("should apply force to particles with finite mass", () => {
         const particle: IParticle = new Particle(zeroVector.clone());
 
         const force = new Vector2(0, 10);
@@ -40,5 +40,78 @@ describe("ParticleForceGenerator", () => {
         expect(particle.getInverseMass()).toEqual(-1);
         expect(particle.getMass()).toEqual(-1);
         expect(particle.getVelocity()).toEqual(new Vector2(0, 0));
+    });
+});
+
+describe("ParticleForceGenerator: Drag", () => {
+    beforeEach(() => {
+        expect(zeroVector).toEqual(new Vector2(0, 0));
+    });
+
+    it("should ignore particles with infinite mass", () => {
+        const particle: IParticle = new Particle(zeroVector.clone(), 0);
+
+        ParticleForceGenerator.drag(particle, 0.003);
+
+        particle.integrate(1);
+
+        expect(particle.getPosition()).toEqual(zeroVector);
+        expect(particle.getAcceleration()).toEqual(zeroVector);
+        expect(particle.getInverseMass()).toEqual(-1);
+        expect(particle.getMass()).toEqual(-1);
+        expect(particle.getVelocity()).toEqual(new Vector2(0, 0));
+    });
+
+    it("should apply force to particles with finite mass", () => {
+        const particle: IParticle = new Particle(zeroVector.clone());
+
+        ParticleForceGenerator.gravity(particle, new Vector2(0, 10));
+        ParticleForceGenerator.drag(particle, 0.003);
+
+        particle.integrate(1);
+
+        expect(particle.getPosition()).toEqual(new Vector2(0, 10));
+        expect(particle.getAcceleration()).toEqual(new Vector2(0, 10));
+        expect(particle.getInverseMass()).toEqual(1);
+        expect(particle.getMass()).toEqual(1);
+        expect(particle.getVelocity()).toEqual(new Vector2(0, 10));
+    });
+});
+
+describe("ParticleForceGenerator: Anchored Spring", () => {
+    it("", () => {
+        const particle: IParticle = new Particle(new Vector2(10, 10), 5);
+        const anchor: IParticle = new Particle(zeroVector.clone());
+
+        ParticleForceGenerator.anchoredSpring(particle, anchor.getPosition(), 5, 300);
+        ParticleForceGenerator.gravity(particle, new Vector2(0, 500));
+
+        particle.integrate(1);
+        anchor.integrate(1);
+
+        expect(particle.getPosition()).toEqual(new Vector2(
+            212.13203435596424,
+            712.1320343559643,
+        ));
+        expect(anchor.getPosition()).toEqual(zeroVector);
+    });
+});
+
+describe("ParticleForceGenerator: Spring", () => {
+    it("", () => {
+        const particle: IParticle = new Particle(new Vector2(10, 10), 5);
+        const anchor: IParticle = new Particle(zeroVector.clone());
+
+        ParticleForceGenerator.spring(particle, anchor, 5, 300);
+        ParticleForceGenerator.gravity(particle, new Vector2(0, 500));
+
+        particle.integrate(1);
+        anchor.integrate(1);
+
+        expect(particle.getPosition()).toEqual(new Vector2(
+            212.13203435596424,
+            712.1320343559643,
+        ));
+        expect(anchor.getPosition()).toEqual(zeroVector);
     });
 });
--- sandbox/src/game.ts
+++ sandbox/src/game.ts
@@ -8,57 +8,76 @@ import {
     ParticleForceGenerator,
     Transform,
     IEntity,
+    IVector2,
 } from "karakuri";
 
 import circle from "../assets/circle.png";
 import square from "../assets/square.png";
 
-class MovableObject extends Behavior {
-    private _speed: number = 500;
+class Ball extends Behavior {
     private _particle!: IParticle;
+    private _anchor: IVector2 | null = null;
+    private _isHolding: boolean = false;
 
     public onStart(): void {
-        this._particle = new Particle(this.transform.position);
+        this._particle = new Particle(this.transform.position, 0.1);
+
+        addEventListener("mousedown", () => {
+            this._isHolding = true;
+        });
+
+        addEventListener("mouseup", () => {
+            this._isHolding = false;
+        });
+
+        addEventListener("mousemove", (event) => {
+            if (!this._isHolding) {
+                return;
+            }
+
+            this.transform.position.add(
+                new Vector2(
+                    event.clientX - this.transform.position.x,
+                    event.clientY - this.transform.position.y,
+                ),
+            );
+        });
     }
 
     public onUpdate(deltaTime: number): void {
-        this._move(deltaTime);
-        ParticleForceGenerator.anchoredSpring(this._particle, new Vector2(), 10, 50);
+        if (this._anchor === null) {
+            return;
+        }
+
+        ParticleForceGenerator.anchoredSpring(this._particle, this._anchor, 5, 300),
         ParticleForceGenerator.gravity(this._particle, new Vector2(0, 500));
         ParticleForceGenerator.drag(this._particle, 0.003);
+
         this._particle.integrate(deltaTime);
     }
 
-    private _move(time: number): void {
-        const left = this.input.isKeyDown("a") ? -1 : 0;
-        const right = this.input.isKeyDown("d") ? 1 : 0;
-        const up = this.input.isKeyDown("w") ? -1 : 0;
-        const down = this.input.isKeyDown("s") ? 1 : 0;
-
-        this._particle.addForce(new Vector2(
-            (left || right) * this._speed * time,
-            (up || down) * this._speed * time,
-        ));
+    public setAnchor(anchor: IVector2): void {
+        this._anchor = anchor;
     }
 }
 
 class Rope extends Behavior {
-    private _anchor: IEntity | null = null;
     private _ball: IEntity | null = null;
+    private _anchor: IEntity | null = null;
+
+    public setBallAndAnchor(ball: IEntity, anchor: IEntity): void {
+        this._ball = ball;
+        this._anchor = anchor;
+    }
 
     public onUpdate(_deltaTime: number): void {
-        if (this._anchor === null || this._ball === null) {
+        if (this._ball === null || this._anchor === null) {
             return;
         }
 
-        const scale = new Vector2(this.transform.scale.x, this._ball.transform.position.y / 100);
+        const ropeScale = new Vector2(this.transform.scale.x, this._ball.transform.position.y / 100);
 
-        this.transform.scale = scale;
-    }
-
-    public setAnchorAndBall(anchor: IEntity, ball: IEntity): void {
-        this._anchor = anchor;
-        this._ball = ball;
+        this.transform.scale = ropeScale;
     }
 }
 
@@ -67,37 +86,43 @@ export async function game(): Promise<void> {
     await engine.init();
 
     const level = engine.createScene();
+    const canvasSize = engine.getCanvasSize();
+
+    const anchor = await level.createEntity({
+        transform: new Transform({
+            position: new Vector2(canvasSize.width / 2, 0),
+        }),
+        sprite: new Sprite({
+            path: circle,
+            color: [0, 1, 0, 1],
+        }),
+    });
+
+    const ballBehavior = new Ball();
+    ballBehavior.setAnchor(anchor.transform.position);
 
     const ball = await level.createEntity({
-        behavior: new MovableObject(),
+        transform: new Transform({
+            position: new Vector2(anchor.transform.position.x, 100),
+        }),
         sprite: new Sprite({
             path: circle,
             color: [1, 0, 0, 1],
         }),
-    });
-
-    const anchor = await level.createEntity({
-        sprite: new Sprite({
-            path: circle,
-            color: [0, 0, 1, 1],
-        }),
-        transform: new Transform({
-            position: new Vector2(25, 0),
-            scale: new Vector2(0.5, 0.5),
-        }),
+        behavior: ballBehavior,
     });
 
     const ropeBehavior = new Rope();
-    ropeBehavior.setAnchorAndBall(anchor, ball);
+    ropeBehavior.setBallAndAnchor(ball, anchor);
 
     await level.createEntity({
+        transform: new Transform({
+            position: new Vector2(anchor.transform.position.x + (anchor.sprite?.clip.width ?? 0) / 2, (anchor.sprite?.clip.height ?? 0) / 2),
+            scale: new Vector2(0.1, 1),
+        }),
         sprite: new Sprite({
             path: square,
-            color: [0, 1, 0, 1],
-        }),
-        transform: new Transform({
-            position: new Vector2(45, 50),
-            scale: new Vector2(0.1, 1),
+            color: [0, 0, 1, 1],
         }),
         behavior: ropeBehavior,
     });
