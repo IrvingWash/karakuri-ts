cda099aa6935c4228ad4cf355ffe26f8b79087ac
Washington Irving <iamwashingyou@gmail.com>
2024-01-01 23:12:49 +0300

    10k particles benchmark

--- sandbox/src/game.ts
+++ sandbox/src/game.ts
@@ -15,22 +15,26 @@ export async function game(): Promise<void> {
     await engine.init();
 
     const level = engine.createScene();
+    const canvasSize = engine.getCanvasSize();
 
-    level.createEntity({
-        particle: new Particle({
-            gravity: PIXELS_PER_METER * 10,
-            mass: 10,
-        }),
-        transform: new Transform({
-            position: new Vector2(500, 0),
-            rotation: new Vector2(45, 0),
-        }),
-        sprite: new Sprite({
-            path: square,
-            antialias: false,
-            color: [1, 0, 0, 1],
-        }),
-    });
+    for (let i = 0; i < 10_000; i++) {
+        await level.createEntity({
+            particle: new Particle({
+                gravity: PIXELS_PER_METER,
+                mass: 0.01,
+            }),
+            transform: new Transform({
+                position: new Vector2(Math.random() * canvasSize.width, Math.random() * canvasSize.height),
+                rotation: new Vector2(0, 0),
+                scale: new Vector2(0.1, 0.1),
+            }),
+            sprite: new Sprite({
+                path: square,
+                antialias: false,
+                color: [Math.random(), Math.random(), Math.random(), 1],
+            }),
+        });
+    }
 
     level.start();
 }
