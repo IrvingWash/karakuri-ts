b55699c2766fefd06ef05ae2bad15fd65de648cf
Washington Irving <iamwashingyou@gmail.com>
2024-02-12 01:20:41 +0300

    add more force generators and tests

--- engine/src/physics/particle/particle-force-generator.ts
+++ engine/src/physics/particle/particle-force-generator.ts
@@ -1,5 +1,6 @@
 import { IVector2, Vector2 } from "../../math/vector2";
 import { IParticle } from "./iparticle";
+import { clamp } from "../../utils/clamp";
 
 const defaultWeightForceConstant = new Vector2(0, 9.8);
 
@@ -12,5 +13,55 @@ export class ParticleForceGenerator {
         }
 
         return constant.toScaled(particle.getMass());
+    }
+
+    public static dragForce(particle: IParticle, constant: number): IVector2 {
+        if (particle.getMass() === 0) {
+            return new Vector2();
+        }
+
+        const squaredMagnitude = particle.getVelocity().getSquaredMagnitude();
+
+        if (squaredMagnitude <= 0) {
+            return new Vector2();
+        }
+
+        const dragDirection = particle.getVelocity().toNormalized().scale(-1);
+
+        const dragMagnitude = constant * squaredMagnitude;
+
+        return dragDirection.scale(dragMagnitude);
+    }
+
+    public static frictionForce(particle: IParticle, constant: number): IVector2 {
+        if (particle.getMass() === 0) {
+            return new Vector2();
+        }
+
+        const frictionDirection = particle.getVelocity().toNormalized().scale(-1);
+
+        return frictionDirection.scale(constant);
+    }
+
+    public static gravitationalForce(
+        particle: IParticle,
+        other: IParticle,
+        constant: number,
+        minDistance: number,
+        maxDistance: number,
+    ): IVector2 {
+        if (particle.getMass() === 0 || other.getMass() === 0) {
+            return new Vector2();
+        }
+
+        const distanceVector = other.getPosition().toSubtracted(particle.getPosition());
+
+        const squaredDistance = clamp(distanceVector.getSquaredMagnitude(), minDistance, maxDistance);
+
+        const attractionDirection = distanceVector.toNormalized();
+
+        const attractionMagnitude = constant * (particle.getMass() * other.getMass()) / squaredDistance;
+
+        return attractionDirection.scale(attractionMagnitude);
     }
 };
--- engine/tests/physics/particle/particle-force-generator.spec.ts
+++ engine/tests/physics/particle/particle-force-generator.spec.ts
@@ -17,4 +17,45 @@ describe("ParticleForceGenerator", () => {
 
         expect(weightForceForZeroMass).toEqual(new Vector2());
     });
+
+    it("should generate drag force", () => {
+        const particle = new Particle({
+            position: new Vector2(10, 10),
+            mass: 3,
+            velocity: new Vector2(10, 10),
+        });
+
+        const dragForce = ParticleForceGenerator.dragForce(particle, 0.1);
+
+        expect(dragForce).toEqual(new Vector2(-14.14213562373095, -14.14213562373095));
+    });
+
+    it("should generate friction force", () => {
+        const particle = new Particle({
+            position: new Vector2(10, 10),
+            mass: 3,
+            velocity: new Vector2(10, 10),
+        });
+
+        const frictionForce = ParticleForceGenerator.frictionForce(particle, 2);
+
+        expect(frictionForce).toEqual(new Vector2(-1.414213562373095, -1.414213562373095));
+    });
+
+    it("should generate gravitational force", () => {
+        const particle = new Particle({
+            position: new Vector2(10, 10),
+            mass: 3,
+            velocity: new Vector2(10, 10),
+        });
+
+        const other = new Particle({
+            position: new Vector2(30, 30),
+            mass: 100,
+        });
+
+        const gravitationalForce = ParticleForceGenerator.gravitationalForce(particle, other, 500, 10, 50);
+
+        expect(gravitationalForce).toEqual(new Vector2(2121.3203435596424, 2121.3203435596424));
+    });
 });
